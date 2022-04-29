dec979cc3932c4fc0f47cb8a1811c1aef4536214
yulia <yulia-11a@yandex.ru>
2022-04-29 19:11:11 +0300

    archive users data before deleting if a parameter with-backup was specified
    (add a limit for creating only 3 users to all scripts for easier debug)

--- batch_create_users.sh
+++ batch_create_users.sh
@@ -3,7 +3,7 @@
 dir=`dirname $0`
 source_file="$dir/users_list.txt"
 
-#iter_num=1
+iter_num=1
 for user_data in $(cat "$source_file"); do
 login=$(echo "$user_data" | awk -F ',' {'print $1'})
 password=$(echo "$user_data" | awk -F ',' {'print $2'})
@@ -30,7 +30,6 @@ echo "done."
 fi
 echo ""
 
-#[ $iter_num -eq 3 ] && exit 8 #FIXME
-
-#iter_num=$(($iter_num+1))
+[ $iter_num -eq 3 ] && exit 8 #FIXME
+iter_num=$(($iter_num+1))
 done
--- batch_delete_users.sh
+++ batch_delete_users.sh
@@ -3,17 +3,17 @@
 dir=`dirname $0`
 source_file="$dir/users_list.txt"
 
+if  [ "$1" == "with-backup" ];then
+# make backup
+tar -zcf /opt/backup.tar /home
+fi
+
 iter_num=1
 for user_data in $(cat "$source_file"); do
 login=$(echo "$user_data" | awk -F ',' {'print $1'})
 
 echo "Delete user with login: $login..."
 
-#  commands
-#if [ ??? ];then
-#make backup of user home
-#tar
-#fi
 deluser --remove-home "$login" &>>/dev/null
 
 if [ $? -ne 0 ];then
@@ -22,8 +22,8 @@ else
 echo "done."
 fi
 echo ""
-[ $iter_num -eq 3 ] && exit 8 #FIXME
 
+[ $iter_num -eq 3 ] && exit 8 #FIXME
 iter_num=$(($iter_num+1))
 done
 
